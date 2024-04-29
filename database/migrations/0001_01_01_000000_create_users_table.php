3a2542737a474d820f8fd1bf37fde325cfdc4a5f
Herianto <52534945+heriantosy@users.noreply.github.com>
2024-04-29 12:17:50 +0700

    Update 0001_01_01_000000_create_users_table.php

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,11 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('username');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role',['customer','admin'])->default('customer');
+             $table->string('ProdiID');
+             $table->string('unit');
+           
+            $table->enum('roles',['user','admin','dosen','mahasiswa'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
